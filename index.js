3818f792eb508e24e1d56e50eff3352a21287299
Joshua Miller <joshsmiller1022@gmail.com>
2021-02-13 20:40:27 -0700

    Postman has errors

---
+++ index.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const express = require('express');
+const massive = require('massive');
+const ctrl = require('./products_controller')
+
+const app = express();
+
+const {SERVER_PORT, CONNECTION_STRING} = process.env;
+
+massive({
+    connectionString: CONNECTION_STRING,
+    ssl: {
+        rejectUnauthorized: false
+    }
+}).then((dbInstance) => {
+    app.set('db', dbInstance);
+    console.log('Database all good')
+}).catch(err => console.log(err));
+
+app.use(express.json());
+
+app.post('/api/products', ctrl.create)
+app.get('/api/products', ctrl.getOne)
+app.get('/api/products/:id', ctrl.getAll)
+app.put('/api/products/:id', ctrl.update)
+app.delete('/api/products/:id', ctrl.delete)
+
+
+app.listen(SERVER_PORT, () => {
+    console.log(`Server listening on port ${SERVER_PORT}`)
+});
